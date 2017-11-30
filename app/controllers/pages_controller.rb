a7d86791b400ae4967e06abe817dd6b516324b50
Anthony Leon <anthonyleon@me.com>
2017-11-30 15:31:26 -0500

    completed home page, contact us page is running with out map or form data, services is still template

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,12 @@ class PagesController < ApplicationController
   def show
     render template: "pages/#{params[:page]}"
   end
+  
+  def services
+    render template: "pages/#{params[:page]}"
+  end
+  
+  def contactus
+    render template: "pages/#{params[:page]}"
+  end
 end
